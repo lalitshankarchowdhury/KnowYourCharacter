2e82348d00a4ea2c072c627dfbe29d939db81584
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2022-10-06 20:55:52 +0530

    Initialize layers with random parameters

--- src/main.c
+++ src/main.c
@@ -5,10 +5,9 @@
 
 int main()
 {
-    float X[BATCHSIZE][4] = { { 1, 2, 3, 2.5 }, { 2, 5, -1, 2 }, { -1.5, 2.7, 3.3, -0.8 }, { 4.32, 3.31, 2.46, -1.53 }, { 1.7, 4.4, 3.2, 2.9 } };
     Matrix2DDefn(float) inputs1;
     Matrix2DInit(inputs1, BATCHSIZE, 4);
-    Matrix2DFill(inputs1, X);
+    Matrix2DRand(inputs1);
     LayerDenseDefn(float) layer1, layer2;
     LayerDenseInit(layer1, 4, 5);
     LayerDenseInit(layer2, 5, 2);
