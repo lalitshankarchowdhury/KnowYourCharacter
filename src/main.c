cbd641c7da85071e3b43e07c1273d994bd9a0f55
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2022-10-06 19:37:46 +0530

    Accept Matrix2D instead of 2D array

--- src/main.c
+++ src/main.c
@@ -8,15 +8,19 @@ int main()
     float X[BATCHSIZE][4] = { { 1, 2, 3, 2.5 }, { 2, 5, -1, 2 }, { -1.5, 2.7, 3.3, -0.8 } };
     float weights1[3][4] = { { 0.2, 0.8, -0.5, 1 }, { 0.5, -0.91, 0.26, -0.5 }, { -0.26, -0.27, 0.17, 0.87 } };
     float biases1[1][3] = { { 2, 3, 0.5 } };
+    Matrix2DDefn(float) inputs1;
+    Matrix2DInit(inputs1, BATCHSIZE, 4);
+    Matrix2DFill(inputs1, X);
     LayerDenseDefn(float) layer1;
     LayerDenseInit(layer1, 4, 3, weights1, biases1);
     puts("Layer 1 weights:");
     Matrix2DDisp(layer1.lweights, "%f");
     puts("Layer 1 biases:");
     Matrix2DDisp(layer1.lbiases, "%f");
-    LayerDenseFrwd(layer1, X);
+    LayerDenseFrwd(layer1, inputs1);
     puts("Layer 1 outputs:");
     Matrix2DDisp(layer1.loutputs, "%f");
     LayerDenseFree(layer1);
+    Matrix2DFree(inputs1);
     return EXIT_SUCCESS;
 }
--- src/neuron/layer.h
+++ src/neuron/layer.h
@@ -8,26 +8,22 @@
         Matrix2DDefn(type) lweights, lbiases, loutputs; \
     }
 
-#define LayerDenseInit(self, n_inputs, n_neurons, l_weights, l_biases) \
-    {                                                                  \
-        self.ninputs = n_inputs;                                       \
-        self.nneurons = n_neurons;                                     \
-        Matrix2DInit(self.lweights, n_neurons, n_inputs);              \
-        Matrix2DInit(self.lbiases, 1, n_neurons);                      \
-        Matrix2DInit(self.loutputs, BATCHSIZE, n_neurons);             \
-        Matrix2DFill(self.lweights, l_weights);                        \
-        Matrix2DFill(self.lbiases, l_biases);                          \
+#define LayerDenseInit(self, n_inputs, n_neurons, weights, biases) \
+    {                                                              \
+        self.ninputs = n_inputs;                                   \
+        self.nneurons = n_neurons;                                 \
+        Matrix2DInit(self.lweights, n_neurons, n_inputs);          \
+        Matrix2DInit(self.lbiases, 1, n_neurons);                  \
+        Matrix2DInit(self.loutputs, BATCHSIZE, n_neurons);         \
+        Matrix2DFill(self.lweights, weights);                      \
+        Matrix2DFill(self.lbiases, biases);                        \
     }
 
 #define LayerDenseFrwd(self, l_input)                             \
     {                                                             \
-        Matrix2DDefn(float) linput;                               \
-        Matrix2DInit(linput, BATCHSIZE, self.ninputs);            \
-        Matrix2DFill(linput, l_input);                            \
         Matrix2DTrsp(self.lweights);                              \
-        Matrix2DMult(linput, self.lweights, self.loutputs);       \
+        Matrix2DMult(l_input, self.lweights, self.loutputs);      \
         VectorRowAdd(self.loutputs, self.lbiases, self.loutputs); \
-        Matrix2DFree(linput);                                     \
     }
 
 #define LayerDenseFree(self)         \
